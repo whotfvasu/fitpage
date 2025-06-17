db5d745964225889e45cf49ce3825c0be7c1192a
Vasu Parashar <136962787+whotfvasu@users.noreply.github.com>
2025-06-17 17:50:58 +0530

    added cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,17 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import routes from './routes/index.js'
+import routes from "./routes/index.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://fitpage-frontend.onrender.com",
+    credentials: true, 
+  })
+);
 app.use(bodyParser.json());
 
 app.use("/api", routes);
-
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
